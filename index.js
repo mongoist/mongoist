606673d7760e4f55be601fdb78cb071914ea9d88
Alejandro Dominguez <adborroto90@gmail.com>
2025-04-04 18:13:09 +0200

    fix: update code style and add support for ObjectId without constructor

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const mongodb = require('mongodb');
-const Database = require('./lib/database');
-const Collection = require('./lib/collection');
-const Cursor = require('./lib/cursor');
-const Bulk = require('./lib/bulk');
+const mongodb = require("mongodb");
+const Database = require("./lib/database");
+const Collection = require("./lib/collection");
+const Cursor = require("./lib/cursor");
+const Bulk = require("./lib/bulk");
 
 function isValidCollectionName(name) {
-  return typeof name === 'string' && name &&
-    !(name.includes('$') || name.includes('\0') || name.startsWith('system.'));
+  return (
+    typeof name === "string" &&
+    name &&
+    !(name.includes("$") || name.includes("\0") || name.startsWith("system."))
+  );
 }
 
-module.exports = function(connectionString, options) {
+module.exports = function (connectionString, options) {
   const db = new Database(connectionString, options);
   const dbMethods = Object.create(null);
 
   return new Proxy(db, {
-    get: function(obj, prop) {
+    get: function (obj, prop) {
       const dbProp = obj[prop];
 
-      if (typeof dbProp === 'function') {
+      if (typeof dbProp === "function") {
         //lazily cache function bound to underlying db
         dbMethods[prop] = dbMethods[prop] || dbProp.bind(db);
         return dbMethods[prop];
@@ -28,29 +31,37 @@ module.exports = function(connectionString, options) {
       }
 
       return dbProp;
-    }
+    },
   });
+};
+
+// support for ObjectId without constructor
+function ObjectId(...args) {
+  if (!(this instanceof mongodb.ObjectId)) {
+    return new mongodb.ObjectId(...args);
+  }
+  return mongodb.ObjectId.apply(this, args);
 }
 
 // expose prototypes
-module.exports.Database = Database
-module.exports.Collection = Collection
-module.exports.Cursor = Cursor
-module.exports.Bulk = Bulk
+module.exports.Database = Database;
+module.exports.Collection = Collection;
+module.exports.Cursor = Cursor;
+module.exports.Bulk = Bulk;
 
 // expose bson stuff visible in the shell
-module.exports.Binary = mongodb.Binary
-module.exports.Code = mongodb.Code
-module.exports.DBRef = mongodb.DBRef
-module.exports.Double = mongodb.Double
-module.exports.Long = mongodb.Long
-module.exports.NumberLong = mongodb.Long // Alias for shell compatibility
-module.exports.MinKey = mongodb.MinKey
-module.exports.MaxKey = mongodb.MaxKey
-module.exports.ObjectID = mongodb.ObjectID
-module.exports.ObjectId = mongodb.ObjectId
-module.exports.Symbol = mongodb.Symbol
-module.exports.Timestamp = mongodb.Timestamp
+module.exports.Binary = mongodb.Binary;
+module.exports.Code = mongodb.Code;
+module.exports.DBRef = mongodb.DBRef;
+module.exports.Double = mongodb.Double;
+module.exports.Long = mongodb.Long;
+module.exports.NumberLong = mongodb.Long; // Alias for shell compatibility
+module.exports.MinKey = mongodb.MinKey;
+module.exports.MaxKey = mongodb.MaxKey;
+module.exports.ObjectID = ObjectId;
+module.exports.ObjectId = ObjectId;
+module.exports.Symbol = mongodb.Symbol;
+module.exports.Timestamp = mongodb.Timestamp;
 
 // Add support for default ES6 module imports
-module.exports.default = module.exports
+module.exports.default = module.exports;
